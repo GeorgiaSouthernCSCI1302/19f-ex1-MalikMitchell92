46232c855c61424c573e1647c7a92e2bb8ae8865
mm15413 <mm15413@CEIT2553214X016.ad.georgiasouthern.edu>
2019-08-21 16:53:18 -0400

    Added Space

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -5,8 +5,11 @@ public class FirstClassInGit {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		
 		System.out.println("What is your last name?");
+		
 		String name = input.next();
+		
 		System.out.println("Hi my last name is " + name);
 		
 		//add a statement to display your name in the console
